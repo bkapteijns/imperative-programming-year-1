8776ceb351e913920eb510df914912220388923e
bkapteijns <bram_kapteijns@hotmail.com>
2020-10-14 14:15:54 +0200

    Done mandatory 7

--- IPC031_2020_assignment_7_files/main.cpp
+++ IPC031_2020_assignment_7_files/main.cpp
@@ -59,7 +59,7 @@ int count_total_words(ifstream &infile, string fileName)
   typedef int Counter;
   typedef string InputWord;
 
-  Counter wordAmount;
+  Counter wordAmount = 0;
   InputWord word;
 
   while (true)
@@ -84,13 +84,71 @@ void array_of_words(string words[], int length, ifstream &infile)
 
   Word input;
 
-  infile >> input;
   for (int i = 0; i < length; i++)
   {
-    words[i] = input;
     infile >> input;
+    words[i] = input;
   }
   return;
+}
+
+void array_of_query(string searchWords[], int length, string input)
+{ //preconditions
+  assert(length > 0);
+  //postconditions: we will have created an array of words from the query
+  typedef int Counter;
+
+  Counter j = 0;
+
+  for (int i = 1; i < input.length(); i++)
+  {
+    if (input[i] == ' ')
+    {
+      j++;
+    }
+    else
+    {
+      searchWords[j] = searchWords[j] + input[i];
+    }
+  }
+
+  return;
+}
+
+int correspondence_amount(int wordAmount, int inputAmount, string allWords[], string inputWords[])
+{ // preconditions:
+  assert(wordAmount > 0);
+  // postconditions: we will have returned the amount of times a wordsequence is in a text
+
+  typedef int Counter;
+  typedef bool Correct;
+
+  Correct corresponds = false;
+  Counter correspondences = 0;
+
+  for (int i = 0; i < wordAmount; i++)
+  {
+    if (allWords[i] == inputWords[0])
+    {
+      corresponds = true;
+      for (int j = 0; j < inputAmount; j++)
+      {
+        if (i + j > wordAmount)
+        {
+          corresponds = false;
+        }
+        if (allWords[i + j] != inputWords[j])
+        {
+          corresponds = false;
+        }
+      }
+      if (corresponds)
+      {
+        correspondences++;
+      }
+    }
+  }
+  return correspondences;
 }
 
 void count(string fileName, ifstream &infile)
@@ -106,46 +164,182 @@ void count(string fileName, ifstream &infile)
   const Counter wordAmount = count_total_words(infile, fileName);
   Word allWords[wordAmount];
   array_of_words(allWords, wordAmount, infile);
-  Word inputWords[10];
-  Counter inputIndex = 0;
-  Counter countAmount = 0;
-  Correct wordInWords = false;
-  InputChar c;
-
-  while (!cin.fail())
+  Counter inputAmount = 0;
+  Word input;
+  getline(cin, input);
+  for (int j = 0; j < input.length(); j++)
   {
-    cin.get(c);
-    if (c == ' ')
+    if (input[j] == ' ')
     {
-      inputIndex++;
-    }
-    else
-    {
-      inputWords[inputIndex] = inputWords[inputIndex] + c;
+      inputAmount++;
     }
   }
+  Word inputWords[inputAmount];
+  array_of_query(inputWords, inputAmount, input);
+
+  Counter correspondences = 0;
+
+  correspondences = correspondence_amount(wordAmount, inputAmount, allWords, inputWords);
+
+  cout << correspondences << '\t' << wordAmount << '\t' << correspondences * 1.0 / wordAmount << endl;
+
+  infile.clear();
+  infile.close();
+  infile.open(fileName);
+
+  return;
+}
+
+void where(ifstream &infile, string fileName)
+{ //preconditions
+  assert(infile.is_open());
+  //postconditions: we will have printed the indices and frequency of a word combo
+  typedef int Counter;
+  typedef string Word;
+  typedef bool Correct;
+  typedef char InputChar;
+  typedef int Index;
+
+  const Counter wordAmount = count_total_words(infile, fileName);
+  Word allWords[wordAmount];
+  array_of_words(allWords, wordAmount, infile);
+  Counter inputAmount = 0;
+  Word input;
+  getline(cin, input);
+  for (int j = 0; j < input.length(); j++)
+  {
+    if (input[j] == ' ')
+    {
+      inputAmount++;
+    }
+  }
+  Word inputWords[inputAmount];
+  array_of_query(inputWords, inputAmount, input);
+
+  Correct corresponds = false;
+  Counter correspondences = 0;
+
+  correspondences = correspondence_amount(wordAmount, inputAmount, allWords, inputWords);
+
+  Index wordPlaces[correspondences];
+  Counter placeIndex = 0;
 
   for (int i = 0; i < wordAmount; i++)
   {
     if (allWords[i] == inputWords[0])
     {
-      wordInWords = true;
-      for (int j = 0; j < inputIndex; j++)
+      corresponds = true;
+      for (int j = 0; j < inputAmount; j++)
       {
-        if (!(allWords[i + j] == inputWords[j]))
+        if (i + j > wordAmount)
         {
-          wordInWords = false;
+          corresponds = false;
+        }
+        if (allWords[i + j] != inputWords[j])
+        {
+          corresponds = false;
         }
       }
-      if (wordInWords)
+      if (corresponds)
       {
-        countAmount++;
-        wordInWords = false;
+        wordPlaces[placeIndex++] = i + 1;
       }
     }
   }
 
-  cout << countAmount << '\t' << wordAmount << '\t' << countAmount * 1.0 / wordAmount << endl;
+  for (int i = 0; i < correspondences; i++)
+  {
+    cout << wordPlaces[i] << ", ";
+  }
+
+  cout << "correspondences: " << correspondences << endl;
+
+  infile.clear();
+  infile.close();
+  infile.open(fileName);
+
+  return;
+}
+
+void context(ifstream &infile, string fileName)
+{ //preconditions
+  assert(infile.is_open());
+  //postconditions: we will have printed the context including the word sequence and frequency of a word sequence
+  typedef int Counter;
+  typedef string Word;
+  typedef bool Correct;
+  typedef char InputChar;
+  typedef int Index;
+  typedef int Length;
+
+  const Counter wordAmount = count_total_words(infile, fileName);
+  Word allWords[wordAmount];
+  array_of_words(allWords, wordAmount, infile);
+  Counter inputAmount = 0;
+  Word input;
+
+  Length contextAmount;
+  cin >> contextAmount;
+
+  getline(cin, input);
+  for (int j = 0; j < input.length(); j++)
+  {
+    if (input[j] == ' ')
+    {
+      inputAmount++;
+    }
+  }
+  Word inputWords[inputAmount];
+  array_of_query(inputWords, inputAmount, input);
+
+  Correct corresponds = false;
+  Counter correspondences = 0;
+
+  correspondences = correspondence_amount(wordAmount, inputAmount, allWords, inputWords);
+
+  Index wordPlaces[correspondences];
+  Counter placeIndex = 0;
+
+  for (int i = 0; i < wordAmount; i++)
+  {
+    if (allWords[i] == inputWords[0])
+    {
+      corresponds = true;
+      for (int j = 0; j < inputAmount; j++)
+      {
+        if (i + j > wordAmount)
+        {
+          corresponds = false;
+        }
+        if (allWords[i + j] != inputWords[j])
+        {
+          corresponds = false;
+        }
+      }
+      if (corresponds)
+      {
+        wordPlaces[placeIndex++] = i;
+      }
+    }
+  }
+
+  for (int j = 0; j < correspondences; j++)
+  {
+    for (int i = 0; i < 2 * contextAmount + inputAmount; i++)
+    {
+      if (wordPlaces[j] + i - contextAmount >= 0 && wordPlaces[j] + i - contextAmount < wordAmount)
+      {
+        cout << allWords[wordPlaces[j] + i - contextAmount] << ' ';
+      }
+    }
+    cout << endl;
+  }
+
+  cout << correspondences << endl;
+
+  infile.clear();
+  infile.close();
+  infile.open(fileName);
 
   return;
 }
@@ -166,6 +360,7 @@ int main()
 
   while (true)
   {
+    cout << "Enter your command" << endl;
     cin >> input;
     if (input == "enter")
     {
@@ -190,13 +385,13 @@ int main()
     }
     else if (input == "where")
     {
-      //where(inputExtra, infile);
+      where(infile, fileName);
     }
     else if (input == "context")
     {
-      //context(inputExtra, infile);
+      context(infile, fileName);
     }
-    else
+    else if (input == "stop")
     {
       return 1;
     }
